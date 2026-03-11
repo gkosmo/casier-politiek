31e407358408be964158eb591e4a6a4e7920014e
george kosmopoulos <gkosmo1@hotmail.com>
2026-03-11 11:27:24 +0100

    feat: install and configure Superglue.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,35 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// Superglue setup
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Application } from '@thoughtbot/superglue'
+import { configureStore } from '@reduxjs/toolkit'
+import { rootReducer } from '@thoughtbot/superglue'
+
+// Import your pages - will be populated in next task
+const pages = {}
+
+// Create Redux store
+const store = configureStore({
+  reducer: rootReducer,
+})
+
+// Initialize Superglue when DOM is ready
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('root')
+  if (container) {
+    const initialPage = JSON.parse(container.dataset.pageData || '{}')
+    const baseUrl = window.location.origin
+    const path = window.location.pathname
+
+    const root = createRoot(container)
+    root.render(
+      <Application
+        initialPage={initialPage}
+        baseUrl={baseUrl}
+        path={path}
+        store={store}
+        mapping={pages}
+      />
+    )
+  }
+})
